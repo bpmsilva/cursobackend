ff95274cb64d65394ecb4628f6d445e9246c21df
Bernardo <bpmsilva@gmail.com>
2026-09-21 16:45:22 -0300

    adiciona operadores-algebricos.php, operadores-logicos.php e estrutura-if.php

---
+++ aulas/aula-02/src/06-operadores-algebricos.php
@@ -0,0 +1,26 @@
+<?php
+    # Operadores algébricos
+    // Adição
+    $soma = 10 + 5;
+    $soma += 5; // Atribuição com adição
+    $soma++; // Incremento
+
+    // Subtração
+    $subtracao = 10 - 5;
+    $subtracao -= 5; // Atribuição com subtração
+    $subtracao--; // Decremento
+
+    // Multiplicação
+    $multiplicacao = 10 * 5;
+    $multiplicacao *= 5; // Atribuição com multiplicação
+
+    // Divisão
+    $divisao = 10 / 5;
+    $divisao /= 5; // Atribuição com divisão
+
+    // Exponenciação
+    $exponenciacao = 10 ** 5;
+    $exponenciacao **= 2; // Atribuição com exponenciação
+
+    // Módulo (ou resto da divisão)
+    $modulo = 10 % 5;
---
+++ aulas/aula-02/src/07-operadores-logicos.php
@@ -0,0 +1,15 @@
+<?php
+    // Operadores de comparação
+    $igual = 10 == 5; // Igualdade
+    $identico = 10 === 5; // Identidade
+    $diferente = 10 != 5; // Diferença
+    $maior = 10 > 5; // Maior que
+    $menor = 10 < 5; // Menor que
+    $maior_igual = 10 >= 5; // Maior ou igual
+    $menor_igual = 10 <= 5; // Menor ou igual
+
+    // Operadores lógicos
+    $e = true && false; // E
+    $ou = true || false; // OU
+    $nao = !true; // NÃO
+?>
